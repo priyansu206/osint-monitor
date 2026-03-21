af69164771642f31e092a6daedcb9b3e9c05cbaf
priyansu206 <priyansup1009@gmail.com>
2026-03-21 21:52:57 +0530

    feat: made the sslchecek automated with full run scan button on the web interface and changed a bit of html file

--- app.py
+++ app.py
@@ -1,43 +1,46 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import subprocess # <-- NEW: Allows Flask to run terminal commands
+import sys        # <-- NEW: Helps Flask find your virtual environment
 
 app = Flask(__name__)
 
-# Helper function to connect to the database securely
 def get_db_connection():
     conn = sqlite3.connect('osint_monitor.db')
     conn.row_factory = sqlite3.Row 
     return conn
 
-# Route 1: homepage that lists all the domains we're monitoring
 @app.route('/')
 def index():
     conn = get_db_connection()
     targets = conn.execute('SELECT * FROM targets').fetchall()
     conn.close()
-
     return render_template('index.html', targets=targets)
 
-# Route 2: Adds a New Domain
 @app.route('/add', methods=['POST'])
 def add():
     new_domain = request.form.get('domain')
-    
     if new_domain:
         clean_domain = new_domain.replace("https://", "").replace("http://", "").strip().strip('/')
-        
         conn = get_db_connection()
         try:
-            
             conn.execute('INSERT INTO targets (domain_name) VALUES (?)', (clean_domain,))
             conn.commit()
         except sqlite3.IntegrityError:
-            
             pass 
         conn.close()
+    return redirect('/')
+
+@app.route('/scan', methods=['POST'])
+def run_scan():
+    print("[FLASK] Triggering background scanner...")
+    try:
+        subprocess.run([sys.executable, 'ssl_checker.py'], check=True)
+        print("[FLASK] Scan complete!")
+    except Exception as e:
+        print(f"[FLASK] Error running scanner: {e}")
         
     return redirect('/')
 
 if __name__ == '__main__':
-    
     app.run(debug=True, port=5000)
